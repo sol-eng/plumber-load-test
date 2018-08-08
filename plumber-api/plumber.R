0dcf93ba166539de3bb6406f513bf5c2ebc85288
Cole Arendt <cole.arendt@outlook.com>
2018-08-08 20:31:19 +0000

    parameterize sleep time add cpu burner computation

--- plumber-api/plumber.R
+++ plumber-api/plumber.R
@@ -4,9 +4,15 @@ library(plumber)
 
 #* Wait 0.2 seconds and then echo back the input
 #* @param msg The message to echo
+#* @param sleep The length of time to sleep (in seconds)
 #* @get /echo
-function(msg = "") {
-    Sys.sleep(0.2)
-    list(msg = paste0("The message is: '", msg, "'"))
+load_func <- function(msg = "", sleep = 0.2) {
+   
+  start <- Sys.time()
+  counter <- 0
+  while (as.double(Sys.time() - start) < sleep) {
+    counter <- counter + 1
+  }
+  list(msg = paste0("The message is: '", msg, "'.  I counted to ",counter))
 }
 
