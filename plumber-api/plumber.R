5439b034b66b14e2003ab90c1676282e6de775db
Cole Arendt <cole.arendt@outlook.com>
2018-08-08 18:47:07 +0000

    update comments

--- plumber-api/plumber.R
+++ plumber-api/plumber.R
@@ -1,15 +1,6 @@
-#
-# This is a Plumber API. You can run the API by clicking
-# the 'Run API' button above.
-#
-# Find out more about building APIs with Plumber here:
-#
-#    https://www.rplumber.io/
-#
-
 library(plumber)
 
-#* @apiTitle Plumber Example API
+#* @apiTitle Plumber Load Test API
 
 #* Wait 0.2 seconds and then echo back the input
 #* @param msg The message to echo
