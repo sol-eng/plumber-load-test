d947d7634aa3cd034e2c7324b9a45c9826e09f19
Cole Arendt <cole.arendt@outlook.com>
2018-08-08 21:00:24 +0000

    add copies add more robust output add parameter types

--- plumber-api/plumber.R
+++ plumber-api/plumber.R
@@ -4,15 +4,23 @@ library(plumber)
 
 #* Wait 0.2 seconds and then echo back the input
 #* @param msg The message to echo
-#* @param sleep The length of time to sleep (in seconds)
+#* @param sleep:numeric The length of time to sleep (in seconds) (default 0.2)
+#* @param copies:numeric The number of times to copy the message (default 1)
 #* @get /echo
-load_func <- function(msg = "", sleep = 0.2) {
+load_func <- function(msg = "", sleep = 0.2, copies = 1) {
    
   start <- Sys.time()
   counter <- 0
+  message("starting counting")
   while (as.double(Sys.time() - start) < sleep) {
     counter <- counter + 1
   }
-  list(msg = paste0("The message is: '", msg, "'.  I counted to ",counter))
+  message("done counting")
+  list(msg = paste0("The message is: '", 
+                    paste(rep(msg, as.integer(copies)), collapse = " ")
+                    , "'")
+       , copies = copies
+       , sleep = sleep
+       , count = paste0("I counted to ",counter))
 }
 
